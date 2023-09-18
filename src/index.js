5fb9b8664616d5cb0e7872c992adb93336f3eb51
Anton <137609245+Ddonec@users.noreply.github.com>
2023-09-19 02:10:06 +0300

    feat: towel sort complede task + notes draft //

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-
 // You should implement your task here.
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    // const stringArray = [].concat(...matrix)
+    // const resultString = '[' + stringArray.join(', ') + ']';
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 != 0) {
+            matrix[i].reverse();
+        } else if (matrix == undefined) {
+            return [];
+        }
+    }
+    return matrix.flat();
+};
+
+
+// console.log(matrix.flat())
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
+
+//   console.log(matrix.length)
+//   let arr = []
+//   let newarr = []
+// for ( i = 0; i < matrix.length; i++){
+//   let n = i + 1
+//   n <= matrix.length
+//   console.log(n)
+// console.log([...matrix[i], ...matrix[n]])
+// newarr = [...matrix[i]]
+// };
+
+// let arr = [].concat(...matrix)
+// arr = JSON.toString(arr)
+
+// console.log(arr)
+
+// const flattenedArray = [].concat(...matrix);
+// const resultString = '[' + flattenedArray.join(',\n ') + ']';
+
+// console.log(resultString);
